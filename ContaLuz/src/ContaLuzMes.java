8f42ffea1cf9b9edcadf2d748262f60ec6cae347
Ana Clara Lopes Hostins <analopeshostins@gmail.com>
2025-09-08 20:57:30 -0300

    Add files via upload

---
+++ ContaLuz/src/ContaLuzMes.java
@@ -0,0 +1,8 @@
+
+public class ContaLuzMes {
+
+	public ContaLuzMes() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ContaLuz/src/ListaAcompanhamento.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class ListaAcompanhamento {
+
+	private List<ContaLuzMes> contasLuz;
+
+	private void organizarValores(List<ContaLuzMes> contasLuz) {
+		for (ContaLuzMes contas : contasLuz) {
+			// fazer bubble sort para organizar valores
+		}
+	}
+	
+	public float menorConsumo() {
+		// TODO
+		return 0;
+	}
+	
+	public float maiorConsumo() {
+		// TODO
+		return 0;
+	}
+	
+	public void addContaLuz() {
+		// TODO
+		
+	}
+	
+	public boolean removerContaLuz() {
+		return false;
+	}
+	
+	public List<ContaLuzMes> listarContas(ContaLuzMes conta){
+		return null;
+	}
+	
+	public ListaAcompanhamento() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
